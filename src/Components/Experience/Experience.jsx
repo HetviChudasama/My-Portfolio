0fd5b47f9bfe4612bf85d5e1e2c1614e5826538e
Hetvi Chudasama <155105217+HetviChudasama@users.noreply.github.com>
2024-12-20 17:09:56 +0530

    Add files via upload

---
+++ src/Components/Experience/Experience.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import './Experience.css';
+
+const Experience = () => {
+  return (
+    <div className="experience">
+      <div className="achievemnet">
+        <div className="circle">2+</div>
+        <span>Years</span>
+        <span>Experience</span>
+      </div>
+      <div className="achievemnet">
+        <div className="circle">20+</div>
+        <span> Completed </span>
+        <span> Projects</span>
+      </div>
+
+      <div className="achievemnet">
+        <div className="circle">5+</div>
+        <span>Companies</span>
+        <span>Work</span>
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
